985d25e7a3f2d8c409efa3948e4bd7bba025d688
Kailas <kailasdoke100@gmail.com>
2026-02-28 09:35:11 +0530

    challange

--- src/Challenges/Book.java
+++ src/Challenges/Book.java
@@ -1,11 +1,67 @@
 package Challenges;
 
 public class Book {
+    static int totalNoOfBooks;
+
     String title;
     String author;
     String isbn;
+    boolean isBorrowed;
 
-    static {
-        
+
+    static{
+        totalNoOfBooks=0;
     }
+
+    {
+        // When the object is created then the total no will become ++
+        totalNoOfBooks++;
+    }
+
+    Book(String isbn, String title, String author ){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+
+    }
+
+    Book(String isbn){
+       this(isbn, "unknown", "unknown");
+   }
+
+    static int getTotalNoOfBooks(){
+       return totalNoOfBooks;
+    }
+
+    void borrowBook(){
+       if(isBorrowed){
+           System.out.println("Book is already borrowed");
+       }else {
+           this.isBorrowed = true;
+       }
+    }
+
+    void returnBook(){
+       if(isBorrowed){
+           this.isBorrowed = false;
+           System.out.println("Hope u enjoyed");
+       }else{
+           System.out.println("this book alread in libray");
+       }
+    }
+
+    public static void main(String[] args) {
+
+       Book B1 = new Book("123");
+       Book B2 = new Book("1234");
+        System.out.println(Book.getTotalNoOfBooks());
+        B1.borrowBook();
+        B2.borrowBook();
+        B1.borrowBook();
+        B1.returnBook();
+        B2.returnBook();
+
+
+    }
+
 }
--- src/chapter7/construc_que.java
+++ src/chapter7/construc_que.java
@@ -2,15 +2,17 @@ package chapter7;
 
 public class construc_que {
      construc_que(){
-         this(5);
+
         System.out.println("A");
 
     }
 
     construc_que(int x){
+         this();
         System.out.println("B");
     }
     public static void main(String[] args){
-        construc_que c1 = new construc_que();
+
+         construc_que c1 = new construc_que(5);
     }
 }
