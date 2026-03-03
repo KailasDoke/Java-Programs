3abfa8470bf5065d746495643a8f77c7d86de4f7
Kailas <kailasdoke100@gmail.com>
2026-03-03 22:26:01 +0530

    Switch case

---
+++ src/chapter8/switchcase.java
@@ -0,0 +1,37 @@
+package chapter8;
+import java.util.Scanner;
+public class switchcase {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Day of the week");
+        System.out.println("Enter the day value between 1 to 7");
+        int day = input.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("wenesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday ");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+
+            default:
+                System.out.println("Invalid day");
+        }
+
+    }
+}
